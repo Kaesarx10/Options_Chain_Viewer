f64a96a351a987adf063ca0a82e26f6dc72f9923
kaesarx10 <venzorcesar@gmail.com>
2024-10-15 12:31:12 -0500

    Added user input for company ticker, and data fetching.

--- options_chain_viewer.py
+++ options_chain_viewer.py
@@ -1,3 +1,16 @@
-# Options Chain Viewer with Python and yfinance.
+# Options Chain Viewer with Python, using libraries yfinance, pandas, and matplotlib. 
 
 import yfinance as yf
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Set up user input to determine company 
+
+ticker = input("Company Ticker: ")
+
+# Fetch Options Chain
+
+stock = yf.Ticker(ticker)
+options_dates = stock.options
+
+print("Available expiration dates:", options_dates)
