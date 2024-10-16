958852140fe2927cf9c91595263f05601e039b1d
kaesarx10 <venzorcesar@gmail.com>
2024-10-15 19:18:22 -0500

    Added print functionality for options chain data, and plot of calls data

--- options_chain_viewer.py
+++ options_chain_viewer.py
@@ -14,3 +14,19 @@ stock = yf.Ticker(ticker)
 options_dates = stock.options
 
 print("Available expiration dates:", options_dates)
+
+# Fetch options data for a specific expiration dates
+
+options_chain = stock.option_chain(expiration_date)
+
+calls = options_chain.calls
+
+puts = options_chain.puts
+
+print("Calls:\n", calls)
+print("Puts:\n", puts)
+
+# Plotting the call Options' strike prices vs their last prices
+
+calls.plot(x='strike', y='lastPrice', kind='bar')
+plt.show()
